9147eed6ee3bf45edab23ccc1e107e2854476a68
storageengineer <36526682+storageengineer@users.noreply.github.com>
2020-10-25 20:52:38 -0500

    Add files via upload
    Separated entry point into Program.cs; it was added to the end of Game.cs

---
+++ MyGameSolution/MyGameProject/Program.cs
@@ -0,0 +1,38 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// Program.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+//
+// dm   10/23/2020      Separated program entry point from Game.cs
+//                      This matches the Monogame template.
+//
+#endregion
+
+#region Using Statements
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace MyGameProject
+{
+    #region Entry Point
+
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    static class Program
+    {
+        static void Main()
+        {
+            using (GameStateManagementGame game = new GameStateManagementGame())
+            {
+                game.Run();
+            }
+        }
+    }
+
+    #endregion
+}
